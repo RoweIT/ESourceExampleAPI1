20b4bf3285148e1490e9a37e7e2a2b6e4504dccd
Adam Nicholas <adam.nicholas@roweit.co.uk>
2019-08-08 15:48:35 +0100

    Updated README, changed buildspec to run tests and use s3 bucket build variable

--- src/app.js
+++ src/app.js
@@ -1,24 +1,24 @@
 let users = [
   'First project!',
   {
-    'id': 1,
-    'Name': 'Adam',
-    'Admin': true
+    id: 1,
+    Name: 'Adam',
+    Admin: true
   },
   {
-    'id': 2,
-    'Name': 'Paul',
-    'Admin': true
+    id: 2,
+    Name: 'Paul',
+    Admin: true
   },
   {
-    'id': 3,
-    'Name': 'Steve',
-    'Admin': true
+    id: 3,
+    Name: 'Steve',
+    Admin: true
   },
   {
-    'id': 4,
-    'Name': 'Gary',
-    'Admin': true
+    id: 4,
+    Name: 'Gary',
+    Admin: true
   }
 ];
 /**
@@ -26,17 +26,17 @@ let users = [
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
  * @param {Object} event - API Gateway Lambda Proxy Input Format
  *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
  * @param {Object} context
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
+ *
  */
 exports.lambdaHandler = async (event, context) => {
   try {
     console.log('Received event:', JSON.stringify(event, null, 2));
-    let responseBody = '';
+    let responseBody;
 
     if (event.pathParameters && event.pathParameters.id) {
       responseBody = getUser(parseInt(event.pathParameters.id));
@@ -44,13 +44,13 @@ exports.lambdaHandler = async (event, context) => {
       responseBody = getAllUsers();
     }
 
-    return response = {
+    return {
       statusCode: 200,
       body: JSON.stringify(responseBody),
       headers: {
         'Access-Control-Allow-Origin': '*'
       }
-    }
+    };
   } catch (err) {
     console.log(err);
     return err;
@@ -61,12 +61,12 @@ const getAllUsers = () => {
   return users;
 };
 
-const getUser = (id) => {
+const getUser = id => {
   let filteredUsers = users.filter(user => user.id === id);
 
-  if (filteredUsers.length > 0) return filteredUsers
+  if (filteredUsers.length > 0) return filteredUsers;
 
   return {
-    'message': 'User not found!'
+    message: 'User not found!'
   };
 };
